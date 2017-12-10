55627418cf56cfa6c9884080fb46820990c099a8
Emre Karahan <e.karahan@jollytur.com>
2017-12-10 16:56:59 +0300

    first commit

---
+++ src/Tests/dynamic-code-console-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dynamic_code_console_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
